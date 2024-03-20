af9d14d41e19b41244afad874a9f6b7370196ad7
kwongyumin <rbals7398@naver.com>
2024-03-20 16:52:04 +0900

    - 프로젝트 Api 공통 Spec 정의 (1)

---
+++ delivery-api/src/main/java/org/delivery/api/common/error/ErrorCode.java
@@ -0,0 +1,21 @@
+package org.delivery.api.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode implements ErrorCodeIfs{
+
+    OK(200 , 200 , "success") ,
+    BAD_REQUEST(400,400, "api bad request") ,
+    SERVER_ERROR(500,500,"server error") ,
+    NULL_POINT_ERROR(500,512,"null point exception")
+
+    ;
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String description;
+
+}
---
+++ delivery-api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,70 @@
+package org.delivery.api.filter;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        var requestMapper = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        var responseMapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        // 실행 전
+        chain.doFilter(requestMapper, responseMapper);
+        // 실행 후
+
+        // 요청 정보
+        var headerNames = requestMapper.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining( headerKey -> {
+            var headerValue = requestMapper.getHeader(headerKey);
+            headerValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(" : ")
+                    .append(headerValue)
+                    .append(" , ")
+                    .append("] ");
+        });
+
+        var requestBody = new String(requestMapper.getContentAsByteArray());
+        var uri = requestMapper.getRequestURI();
+        var method = requestMapper.getMethod();
+
+        log.info(" >>>>> uri : {} , method : {} , header : {} , body : {}",uri, method ,headerValues, requestBody);
+
+        // 응답 정보
+        var responseHeaderValues = new StringBuilder();
+
+        responseMapper.getHeaderNames().forEach(headerKey -> {
+            var headerValue = responseMapper.getHeader(headerKey);
+            responseHeaderValues
+                    .append("[")
+                    .append(headerKey)
+                    .append(" : ")
+                    .append(headerValue)
+                    .append(" , ")
+                    .append("] ");
+        });
+
+        var responseBody = new String(responseMapper.getContentAsByteArray());
+
+        log.info(" <<<<< uri : {} , method : {} , header : {} , body : {}",uri, method ,responseHeaderValues, responseBody);
+        // responseBody 초기화 (필수)
+        responseMapper.copyBodyToResponse();
+    }
+}
