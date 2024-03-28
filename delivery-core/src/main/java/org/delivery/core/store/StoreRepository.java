301428528a676185d0a7e60b03cb1d092f73e007
kwongyumin <rbals7398@naver.com>
2024-03-28 23:31:35 +0900

    - 스토어 도메인 설계

---
+++ delivery-core/src/main/java/org/delivery/core/store/StoreRepository.java
@@ -0,0 +1,18 @@
+package org.delivery.core.store;
+
+import org.delivery.core.store.enums.StoreCategory;
+import org.delivery.core.store.enums.StoreStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+
+    // 유효한 스토어
+    Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreStatus status);
+    // 유효한 스토어 리스트
+    List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
+    // 유요한 특정 카테고리의 스토어 리스트
+    List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus status , StoreCategory category);
+}
