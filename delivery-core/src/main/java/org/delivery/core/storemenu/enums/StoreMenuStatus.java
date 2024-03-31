22375515fa00beae2de16294b22da15f93107671
kwongyumin <rbals7398@naver.com>
2024-03-31 17:14:29 +0900

    - 스토어 메뉴 도메인 &서비스 설계

---
+++ delivery-core/src/main/java/org/delivery/core/storemenu/enums/StoreMenuStatus.java
@@ -0,0 +1,13 @@
+package org.delivery.core.storemenu.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StoreMenuStatus {
+
+    REGISTERED("등록") ,
+    UNREGISTERED("해지"),
+    ;
+
+    private String description;
+}
