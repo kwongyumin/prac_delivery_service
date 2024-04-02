f337f94e0c8358af5caed4107975b24df47bdd46
kwongyumin <rbals7398@naver.com>
2024-04-02 20:04:42 +0900

    - 메뉴 주문 도메인 &서비스 설계 (2)

--- delivery-core/src/main/java/org/delivery/core/userorder/UserOrderRepository.java
+++ delivery-core/src/main/java/org/delivery/core/userorder/UserOrderRepository.java
@@ -1,8 +1,22 @@
 package org.delivery.core.userorder;
 
+import org.delivery.core.userorder.enums.UserOrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface UserOrderRepository extends JpaRepository<UserOrderEntity,Long> {
 
+
+    // 특정 유저의 모든 주문
+    List<UserOrderEntity> findAllByUserIdAndStatusOrderByIdDesc(Long userId , UserOrderStatus status);
+
+    List<UserOrderEntity> findAllByUserIdAndStatusInOrderByIdDesc(Long userId , List<UserOrderStatus> status);
+
+    // 특정 주문에 대한 정보
+    Optional<UserOrderEntity> findAllByIdAndStatusAndUserId(Long id, UserOrderStatus status, Long userId);
+    // 특정 주문에 대한 정보 (상태값 x)
+    Optional<UserOrderEntity> findAllByIdAndUserId(Long id,Long userId);
 
 }
