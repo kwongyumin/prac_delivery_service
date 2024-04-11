c730124c98b4cbd572f8b8fc64501e69d4a88ae5
kwongyumin <rbals7398@naver.com>
2024-04-11 23:23:43 +0900

    - Producer 개발 (1)

--- mysql/initdb.d/create_table.sql
+++ mysql/initdb.d/create_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `store` (
+CREATE TABLE IF NOT EXISTS `delivery`.`store` (
  `id` bigint NOT NULL AUTO_INCREMENT,
  `name` varchar(100) COLLATE utf8mb4_bin NOT NULL,
  `address` varchar(150) COLLATE utf8mb4_bin NOT NULL,
@@ -22,9 +22,23 @@ CREATE TABLE IF NOT EXISTS `delivery`.`store_menu` (
  `thumbnail_url` VARCHAR(200) NOT NULL,
  `like_count` INT NULL DEFAULT 0,
  `sequence` INT NULL DEFAULT 0,
-PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;;
+PRIMARY KEY (`id`)) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `delivery`.`store_user` (
+  `id` bigint(32) NOT NULL,
+  `store_id` bigint(32) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `registered_at` datetime DEFAULT NULL,
+  `unregistered_at` datetime DEFAULT NULL,
+  `last_login_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_store_id` (`store_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS `delivery`.`user` (
  `id` bigint NOT NULL AUTO_INCREMENT,
  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
  `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
@@ -50,7 +64,7 @@ PRIMARY KEY (`id`),
 INDEX `idx_user_id` (`user_id` ASC) VISIBLE
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-CREATE TABLE `user_order_menu` (
+CREATE TABLE IF NOT EXISTS `delivery`.`user_order_menu` (
  `id` bigint NOT NULL AUTO_INCREMENT,
  `user_order_id` bigint NOT NULL,
  `store_menu_id` bigint NOT NULL,
